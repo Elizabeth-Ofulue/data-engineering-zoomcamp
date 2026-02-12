907460ac32f9163dbbfca46c4008088c9c863b27
Elizabeth Ofulue <lizofulue@gmail.com>
2026-02-12 17:47:05 +0200

    Merge remote-tracking branch 'upstream/main'

--- 07-personal-practice/pipeline/ingest_data.py
+++ 07-personal-practice/pipeline/ingest_data.py
@@ -5,6 +5,7 @@ from sys import prefix
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 dtype = {
     "VendorID": "Int64",
@@ -30,16 +31,18 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    year = 2021
-    month = 1
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-    target_table = 'yellow_taxi_data'
-    chunksize = 100000
+@click.command()
+@click.option('--year', default=2021, help='Year for taxi data')
+@click.option('--month', default=1, help='Month for taxi data')
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, help='Chunk size for processing')
+
+def run(year, month, pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, chunksize):
     
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
@@ -72,7 +75,3 @@ def run():
 
 if __name__ == '__main__':
     run()
-
-
-
-
